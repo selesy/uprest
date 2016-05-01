737d0346e42de67639b89a8c479d3359c65fb46e
Steve Moyer <smoyer1@selesy.com>
2016-04-30 21:43:03 -0400

    A marker annotation to indicate we should parse the HttpResponse's entity body to create the parameter's value.

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/annotations/Body.java
@@ -0,0 +1,18 @@
+package com.selesy.testing.uprest.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A "marker annotation" that indicates the annotated method parameter should be
+ * parsed from the HttpResponse's entity body.
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface Body {
+
+}
