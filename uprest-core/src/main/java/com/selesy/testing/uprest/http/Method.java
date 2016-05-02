bfd7f9c0976251dd0b6d31f913c1dcff134b7f1e
Steve Moyer <smoyer1@selesy.com>
2016-05-02 06:39:28 -0400

    Updated this class to produce HttpUriRequests instead of HttpRequests so that the target URI can be part of each test case's payload as it's passed through JUnit5.

--- uprest-core/src/main/java/com/selesy/testing/uprest/http/Method.java
+++ uprest-core/src/main/java/com/selesy/testing/uprest/http/Method.java
@@ -1,6 +1,8 @@
 package com.selesy.testing.uprest.http;
 
-import org.apache.http.HttpRequest;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
@@ -9,10 +11,11 @@ import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpTrace;
+import org.apache.http.client.methods.HttpUriRequest;
 
 /**
  * Provides a fixed list of the HTTP 1.1 Verbs as well as a means of retrieving
- * a new HttpRequest instance for the specified value.
+ * a new HttpUriRequest instance for the specified value.
  * 
  * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
  */
@@ -27,25 +30,37 @@ public enum Method {
   PUT(HttpPut.class),
   TRACE(HttpTrace.class);
 
-  Class<? extends HttpRequest> httpRequestClass;
+  Class<? extends HttpUriRequest> httpUriRequestClass;
 
-  private Method(Class<? extends HttpRequest> httpRequestClass) {
-    this.httpRequestClass = httpRequestClass;
+  private Method(Class<? extends HttpUriRequest> httpUriRequestClass) {
+    this.httpUriRequestClass = httpUriRequestClass;
   }
 
   /**
    * Provides an instance of the HttpRequest appropriate to the value of this
    * Method instance.
    * 
-   * @return An HttpRequest.
+   * @param uri
+   *          A String representation of the URI that this method will target.
+   * @return An HttpUriRequest.
    * @throws InstantiationException
    *           When an instance of the HttpRequest cannot be created.
    * @throws IllegalAccessException
    *           When the current process does not have permission to create an
    *           instance of the HttpRequest object.
+   * @throws SecurityException
+   *           When the selected constructor is not available with a public
+   *           scope.
+   * @throws NoSuchMethodException
+   *           When the selected constructor does not exist.
+   * @throws InvocationTargetException
+   *           When an error occurs invoking the target method.
+   * @throws IllegalArgumentException
+   *           When the passed URI is not a string.
    */
-  public HttpRequest getHttpRequest() throws InstantiationException, IllegalAccessException {
-    return httpRequestClass.newInstance();
+  public HttpUriRequest getHttpUriRequest(String uri) throws InstantiationException, IllegalAccessException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
+    Constructor<? extends HttpUriRequest> constructor = httpUriRequestClass.getConstructor(String.class);
+    return constructor.newInstance(uri);
   }
 
 }
