9d0223e3d74eeb1aff8c046906170268c7ce9315
Steve Moyer <smoyer1@selesy.com>
2016-05-03 13:59:11 -0400

    Made the class simpler and faster by replacing the switch statement with a lookup from a map.

--- uprest-javax-json/src/main/java/com/selesy/testing/uprest/javax/json/UpRestJavaxJson.java
+++ uprest-javax-json/src/main/java/com/selesy/testing/uprest/javax/json/UpRestJavaxJson.java
@@ -4,9 +4,8 @@
 package com.selesy.testing.uprest.javax.json;
 
 import java.lang.reflect.Parameter;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
@@ -25,50 +24,62 @@ import com.selesy.testing.uprest.utilities.BodyAnnotationProcessing;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Supports resolution of JsonObject and JsonArray parameters that are
- * annotated with @EntityBody.
+ * Supports resolution of JsonObject and JsonArray parameters that are annotated
+ * with @EntityBody to allow javax-json processing.
  * 
  * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
  */
 @Slf4j
 public class UpRestJavaxJson implements MethodParameterResolver {
-  
-  public static final Set<Class<?>> SUPPORTED_BODY_CLASSES = new HashSet<>(Arrays.asList(
-      JsonArray.class,
-      JsonObject.class
-  ));
-  
-  /* (non-Javadoc)
-   * @see org.junit.gen5.api.extension.MethodParameterResolver#resolve(java.lang.reflect.Parameter, org.junit.gen5.api.extension.MethodInvocationContext, org.junit.gen5.api.extension.ExtensionContext)
+
+  public static final Map<Class<?>, Class<? extends ChainableParameterResolver>> SUPPORTED_BODY_CLASSES = new HashMap<>();
+
+  static {
+    SUPPORTED_BODY_CLASSES.put(JsonArray.class, JsonArrayEntityBodyResolver.class);
+    SUPPORTED_BODY_CLASSES.put(JsonObject.class, JsonObjectEntityBodyResolver.class);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.junit.gen5.api.extension.MethodParameterResolver#resolve(java.lang.
+   * reflect.Parameter, org.junit.gen5.api.extension.MethodInvocationContext,
+   * org.junit.gen5.api.extension.ExtensionContext)
    */
   @Override
   public Object resolve(Parameter parameter, MethodInvocationContext mic, ExtensionContext ec) throws ParameterResolutionException {
     log.trace("resolve()");
 
-    String className = parameter.getType().getName();
-    log.debug("Class name: {}", className);
-    ChainableParameterResolver resolver = new NullResolver();
+    Class<?> parameterClass = parameter.getType();
+    log.debug("Class name: {}", parameterClass.getName());
 
-    switch (className) {
-    case "javax.json.JsonObject":
-      resolver = new JsonObjectEntityBodyResolver();
-      break;
-    case "javax.json.JsonArray":
-      resolver = new JsonArrayEntityBodyResolver();
-      break;
-    default:
-      break;
+    ChainableParameterResolver resolver = new NullResolver();
+    if (SUPPORTED_BODY_CLASSES.containsKey(parameterClass)) {
+      try {
+        resolver = SUPPORTED_BODY_CLASSES.get(parameterClass).newInstance();
+      } catch (IllegalAccessException e) {
+        log.error("Could not access parameter resolver class.");
+      } catch (InstantiationException e) {
+        log.error("Could not instantiate parameter resolver class.");
+      }
     }
+    log.debug("Body parameter resolver class: {}", resolver.getClass().getName());
 
     return resolver.resolve(mic, ec);
   }
 
-  /* (non-Javadoc)
-   * @see org.junit.gen5.api.extension.MethodParameterResolver#supports(java.lang.reflect.Parameter, org.junit.gen5.api.extension.MethodInvocationContext, org.junit.gen5.api.extension.ExtensionContext)
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.junit.gen5.api.extension.MethodParameterResolver#supports(java.lang.
+   * reflect.Parameter, org.junit.gen5.api.extension.MethodInvocationContext,
+   * org.junit.gen5.api.extension.ExtensionContext)
    */
   @Override
   public boolean supports(Parameter parameter, MethodInvocationContext arg1, ExtensionContext arg2) throws ParameterResolutionException {
-    return BodyAnnotationProcessing.supportsBody(parameter, SUPPORTED_BODY_CLASSES);
+    return BodyAnnotationProcessing.supportsBody(parameter, SUPPORTED_BODY_CLASSES.keySet());
   }
 
 }
