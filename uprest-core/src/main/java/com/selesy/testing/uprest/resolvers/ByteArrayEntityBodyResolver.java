fdea30ac83193ef0db542f484ee36aa50752b427
Steve Moyer <smoyer1@selesy.com>
2016-05-03 14:59:15 -0400

    Two of the basic EntityBody resolvers.

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/resolvers/ByteArrayEntityBodyResolver.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.selesy.testing.uprest.resolvers;
+
+import org.junit.gen5.api.extension.ExtensionContext;
+import org.junit.gen5.api.extension.MethodInvocationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * A pass-through resolver for HTTP entity bodies that are retrieved
+ * as a byte[]
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+@Slf4j
+public class ByteArrayEntityBodyResolver extends EntityBodyResolver {
+
+  /**
+   * Simply passes the stored EntityBody byte[] (available from the abstract
+   * EntityBodyResolver through to the MethodParameterResolver.
+   * 
+   * @param mic
+   *          The MethodInvocationContext.
+   * @param ec
+   *          The ExtensionContext.
+   * 
+   * @return The resolved byte[] parameter.
+   * 
+   * @see com.selesy.testing.uprest.resolvers.ChainableParameterResolver#resolve(org.
+   *      junit.gen5.api.extension.MethodInvocationContext,
+   *      org.junit.gen5.api.extension.ExtensionContext)
+   */
+  @Override
+  public Object resolve(MethodInvocationContext mic, ExtensionContext ec) {
+    log.trace("resolve()");
+
+    byte[] entityBody = (byte[]) super.resolve(mic, ec);
+    log.debug("Entity body as byte[]: {}", entityBody);
+
+    return entityBody;
+  }
+
+}
---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/resolvers/StringEntityBodyResolver.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.selesy.testing.uprest.resolvers;
+
+import org.junit.gen5.api.extension.ExtensionContext;
+import org.junit.gen5.api.extension.MethodInvocationContext;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Provides method parameter resolution for parameters that specify a
+ * String @EntityBody.
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+@Slf4j
+public class StringEntityBodyResolver extends EntityBodyResolver {
+
+  /**
+   * Resolves @EntityBody parameters that are specified as String types.
+   * 
+   * @param mic
+   *          The MethodInvocationContext.
+   * @param ec
+   *          The ExtensionContext.
+   * 
+   * @return The resolved String parameter.
+   * 
+   * @see com.selesy.testing.uprest.resolvers.ChainableParameterResolver#resolve(org.junit.gen5.api.extension.MethodInvocationContext,
+   *      org.junit.gen5.api.extension.ExtensionContext)
+   */
+  @Override
+  public Object resolve(MethodInvocationContext mic, ExtensionContext ec) {
+    log.trace("resolve()");
+
+    byte[] entityBody = (byte[]) super.resolve(mic, ec);
+    String stringEntityBody = new String(entityBody);
+    log.debug("Entity body: {}", stringEntityBody);
+
+    return stringEntityBody;
+  }
+
+}
