4648d455551278c4388461a1b06c6b146bf4c6b4
Steve Moyer <smoyer1@selesy.com>
2016-05-02 22:31:29 -0400

    Added MethodParameterResolution for the Performance class.

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/resolvers/PerformanceResolver.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.selesy.testing.uprest.resolvers;
+
+import org.junit.gen5.api.extension.ExtensionContext;
+import org.junit.gen5.api.extension.ExtensionContext.Store;
+import org.junit.gen5.api.extension.MethodInvocationContext;
+
+import com.selesy.testing.uprest.UpRest;
+import com.selesy.testing.uprest.http.Performance;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * If a request has already been made, retrieves the stored Performance data,
+ * otherwise it chains to the HttpResponseResolver (which may also cause an HTTP
+ * request to be created).
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+@Slf4j
+public class PerformanceResolver implements ChainableParameterResolver {
+
+  /**
+   * Uses the MethodInvocationContext and ExtensionContext to retrieve or create
+   * a Performance instance for this test.
+   * 
+   * @param mic
+   *          The MethodInvocationContext
+   * @param ec
+   *          The ExtensionContext
+   * @return A Performance instance describing the request/response cycle
+   *         associated with this test.
+   * @see com.selesy.testing.uprest.resolvers.ChainableParameterResolver#resolve(org.
+   *      junit.gen5.api.extension.MethodInvocationContext,
+   *      org.junit.gen5.api.extension.ExtensionContext)
+   */
+  @Override
+  public Object resolve(MethodInvocationContext mic, ExtensionContext ec) {
+    log.trace("resolve()");
+
+    Store store = ec.getStore();
+    Performance performance = (Performance) store.getOrComputeIfAbsent(UpRest.STORE_KEY_PERFORMANCE, (c) -> {
+      HttpResponseResolver httpResponseResolver = new HttpResponseResolver();
+      httpResponseResolver.resolve(mic, ec);
+      return store.get(UpRest.STORE_KEY_PERFORMANCE);
+    });
+
+    return performance;
+  }
+
+}
