c6bd351f2688ac3bd789628db784dde39e29d393
Steve Moyer <smoyer1@selesy.com>
2016-05-03 08:56:18 -0400

    The default ChainableParameterResolver.

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/resolvers/NullResolver.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.selesy.testing.uprest.resolvers;
+
+import org.junit.gen5.api.extension.ExtensionContext;
+import org.junit.gen5.api.extension.MethodInvocationContext;
+
+/**
+ * The default resolver which returns a null value for any unsupported parameter
+ * types.
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+public class NullResolver implements ChainableParameterResolver {
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.selesy.testing.uprest.resolvers.ChainableParameterResolver#resolve(org.
+   * junit.gen5.api.extension.MethodInvocationContext,
+   * org.junit.gen5.api.extension.ExtensionContext)
+   */
+  @Override
+  public Object resolve(MethodInvocationContext mic, ExtensionContext ec) {
+    return null;
+  }
+
+}
