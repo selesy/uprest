9fc1aad48519fd21dd84d07ad909f6519b82e0d7
Steve Moyer <smoyer1@selesy.com>
2016-05-05 14:12:21 -0400

    Added the StatusFamily enum to represent the HTTP 1.1 status families as defined in RFC2616.

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/http/StatusFamily.java
@@ -0,0 +1,83 @@
+package com.selesy.testing.uprest.http;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.apache.http.StatusLine;
+
+import lombok.Getter;
+
+/**
+ * Represents the HTTP status code families according to section 10 of the HTTP
+ * 1.1 specification defined by RFC2616
+ * (https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html).
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+@Getter
+public enum StatusFamily {
+
+  INFORMATIONAL(1, "Informational"),
+  OK(2, "OK"),
+  REDIRECTION(3, "Redirection"),
+  CLIENT_ERROR(4, "Client Error"),
+  SERVER_ERROR(5, "Server Error");
+
+  static final int STATUS_CODE_MINIMUM = 100;
+  static final int STATUS_CODE_MAXIMUM = 599;
+
+  static final String MESSAGE_STATUSCODE_RANGE = "The HTTP status code must be between " +
+      STATUS_CODE_MINIMUM + " and " + STATUS_CODE_MAXIMUM + " (inclusive).";
+  static final String MESSAGE_STATUSLINE_NULL = "The statusLine parameter may not by null.";
+
+  static final Map<Integer, StatusFamily> reverseMapFromStatusCode = new HashMap<>();
+
+  static {
+    // Builds a reverse map so that the StatusFamily can be looked up based
+    // on the associated HTTP 1.1 status codes.
+    Stream.of(StatusFamily.values()).forEach((f) -> reverseMapFromStatusCode.put(f.familyCode, f));
+  }
+
+  int familyCode;
+  String familyMessage;
+
+  private StatusFamily(int familyCode, String familyMessage) {
+    this.familyCode = familyCode;
+    this.familyMessage = familyMessage;
+  }
+
+  /**
+   * Retrieves the StatusFamily for the provided HTTP status code.
+   * 
+   * @param statusCode
+   *          The HTTP status code.
+   * @return The StatusFamily associated with the provided HTTP status code.
+   * @throws IllegalArgumentException
+   *           When the statusCode parameter is outside the valid range for HTTP
+   *           status codes (100 <-> 599 inclusive).
+   */
+  public static StatusFamily fromStatusCode(int statusCode) {
+    if (statusCode < STATUS_CODE_MINIMUM || statusCode > STATUS_CODE_MAXIMUM) {
+      throw new IllegalArgumentException(MESSAGE_STATUSCODE_RANGE);
+    }
+    return reverseMapFromStatusCode.get(statusCode / 100);
+  }
+
+  /**
+   * Retrieves the StatusFamily for the provided StatusLine.
+   * 
+   * @param statusLine
+   *          The StatusLine containing an HTTP status code.
+   * @return The StatusFamily associated with the provided StatusLine.
+   * @throws IllegalArgumentException
+   *           When the statusLine parameter is null.
+   */
+  public static StatusFamily fromStatusLine(StatusLine statusLine) {
+    if (statusLine == null) {
+      throw new IllegalArgumentException(MESSAGE_STATUSLINE_NULL);
+    }
+    return StatusFamily.fromStatusCode(statusLine.getStatusCode());
+  }
+
+}
---
+++ uprest-core/src/test/java/com/selesy/testing/uprest/http/StatusFamilyTest.java
@@ -0,0 +1,80 @@
+package com.selesy.testing.uprest.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.gen5.api.Assertions.assertEquals;
+import static org.junit.gen5.api.Assertions.expectThrows;
+
+import org.apache.http.ProtocolVersion;
+import org.apache.http.StatusLine;
+import org.apache.http.message.BasicStatusLine;
+import org.junit.gen5.api.Test;
+
+/**
+ * Tests for the StatusFamily enum.
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+public class StatusFamilyTest {
+
+  /**
+   * Tests that a valid StatusFamily can be retrieved via reverse lookup from a
+   * valid status code.
+   */
+  @Test
+  public void testFromStatusCodeWithValidCode() {
+    assertEquals(StatusFamily.OK, StatusFamily.fromStatusCode(201));
+  }
+
+  /**
+   * Tests that an IllegalArgumentException is thrown when a lookup is attempted
+   * using a status code below the allowed minimum.
+   */
+  @Test
+  public void testFromStatusCodeWithLowCode() {
+    Throwable thrown = expectThrows(IllegalArgumentException.class, () -> {
+      StatusFamily.fromStatusCode(99);
+    });
+    assertThat(thrown)
+        .hasMessage(StatusFamily.MESSAGE_STATUSCODE_RANGE);
+  }
+
+  /**
+   * Tests that an IllegalArgumentException is thrown when a lookup is attempted
+   * using a status code above the allowed maximum.
+   */
+  @Test
+  public void testFromStatusCodeWithHighCode() {
+    Throwable thrown = expectThrows(IllegalArgumentException.class, () -> {
+      StatusFamily.fromStatusCode(99);
+    });
+    assertThat(thrown)
+        .hasMessage(StatusFamily.MESSAGE_STATUSCODE_RANGE);
+  }
+
+  /**
+   * Tests that a valid StatusFamily can be retrieved via reverse lookup from a
+   * valid StatusLine.
+   */
+  @Test
+  public void testFromStatusLine() {
+    ProtocolVersion protocolVersion = new ProtocolVersion("HTTP", 1, 1);
+    StatusLine statusLine = new BasicStatusLine(protocolVersion, 201, "Created");
+    StatusFamily statusFamily = StatusFamily.fromStatusLine(statusLine);
+    assertThat(statusFamily)
+        .isEqualTo(StatusFamily.OK);
+  }
+
+  /**
+   * Tests that an IllegalArgumentException is thrown when a lookup is attempted
+   * passing a null StatusLine parameter.
+   */
+  @Test
+  public void testFromStatusLineWithNull() {
+    Throwable thrown = expectThrows(IllegalArgumentException.class, () -> {
+      StatusFamily.fromStatusLine(null);
+    });
+    assertThat(thrown)
+        .hasMessage(StatusFamily.MESSAGE_STATUSLINE_NULL);
+  }
+
+}
