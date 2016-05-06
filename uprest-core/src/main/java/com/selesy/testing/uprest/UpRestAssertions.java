d924057b36ee74b4653b80ba8c597e621b0090dc
Steve Moyer <smoyer1@selesy.com>
2016-05-06 10:33:09 -0400

    Created the collection of Assertions for upREST with the first sub-assertion - assertThat(StatusLine actual).

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/UpRestAssertions.java
@@ -0,0 +1,27 @@
+package com.selesy.testing.uprest;
+
+import org.apache.http.StatusLine;
+
+import com.selesy.testing.uprest.assertions.StatusLineAssert;
+
+/**
+ * A collection of fluent (AssertJ) style assertions that provide a single
+ * point of access for the static execution of assertThat() methods for
+ * upREST specific classes.
+ * 
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
+public class UpRestAssertions {
+  
+  /**
+   * Delegates the creation of a StatusLineAssert object for verifying
+   * StatusLine objects.
+   * 
+   * @param actual The target StatusLine object.
+   * @return A StatusLineAssert object for method chaining.
+   */
+  public static StatusLineAssert assertThat(StatusLine actual) {
+    return StatusLineAssert.assertThat(actual);
+  }
+
+}
