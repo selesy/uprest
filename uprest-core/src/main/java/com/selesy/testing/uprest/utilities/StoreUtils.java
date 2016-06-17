90d742e04a73e6058c8fb83d5d822f6483b6f813
Steve Moyer <smoyer@psu.edu>
2016-06-17 12:49:31 -0400

    Added JSR305 annotations and javadocs.

--- uprest-core/src/main/java/com/selesy/testing/uprest/utilities/StoreUtils.java
+++ uprest-core/src/main/java/com/selesy/testing/uprest/utilities/StoreUtils.java
@@ -1,15 +1,30 @@
 package com.selesy.testing.uprest.utilities;
 
+import javax.annotation.Nonnull;
+
 import org.junit.gen5.api.extension.ExtensionContext;
 import org.junit.gen5.api.extension.ExtensionContext.Namespace;
 import org.junit.gen5.api.extension.ExtensionContext.Store;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility methods to make working with an ExtensionContext's Store easier.
+ *
+ * @author Steve Moyer &lt;smoyer1@selesy.com&gt;
+ */
 @UtilityClass
 public class StoreUtils {
 
-  public static Store getNamespacedStore(ExtensionContext extensionContext) {
+  /**
+   * Uses the ExtensionContext's UniqueId to retrieve a Namespace(d) store.
+   * 
+   * @param extensionContext
+   *          the current ExtensionContext.
+   * @return a unique Store for this test instance.
+   */
+  @Nonnull
+  public static Store getNamespacedStore(@Nonnull ExtensionContext extensionContext) {
     String unigueId = extensionContext.getUniqueId();
     Namespace namespace = Namespace.of(unigueId);
     return extensionContext.getStore(namespace);
