51dad4788713fa57d819281b0c5fde5711df8809
Steve Moyer <smoyer@psu.edu>
2016-06-29 09:44:44 -0400

    Added a Constants class which will eventually replace UpRestOld.java.
    Currently it defines the character encoding for String to byte[]
    conversion (and back).

---
+++ uprest-core/src/main/java/com/selesy/testing/uprest/configuration/Constants.java
@@ -0,0 +1,12 @@
+package com.selesy.testing.uprest.configuration;
+
+import java.nio.charset.Charset;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+  public static final Charset CHARSET = Charset.forName("UTF-8");
+  
+}
